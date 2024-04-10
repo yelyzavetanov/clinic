f0dd0589c354712de279a62bbb5cd95ef5f460a3
yelyzavetanov <yelyzavetanov@gmail.com>
2024-04-10 16:59:18 +0300

    Make logo

--- src/components/common/Logo/Logo.jsx
+++ src/components/common/Logo/Logo.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import s from "./Logo.module.css";
+import logo from "../../../logo.svg";
 
 function Logo() {
     return (
         <div className={s.logo}>
-            <div className={s.logoImg}>L</div>
-            <div className={s.title}>Clinic</div>
+            <div className={s.logoImg}><img src={logo}/></div>
+            <div className={s.title}>Clinic
+                {/*<span>PRO</span>*/}
+                </div>
         </div>
     )
 }
