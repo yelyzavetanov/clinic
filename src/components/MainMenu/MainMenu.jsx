80a0e987df0599f27ca09fc0d580ff48fccce6c5
NazarKhmil <nazarkhmil06@gmail.com>
2024-04-11 11:32:40 +0300

    Rebase

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -4,8 +4,18 @@ import Logo from "../common/Logo/Logo";
 
 function MainMenu() {
     return (
-        <div>
-            <Logo/>
+        <div className={s.mainMenu}>
+            <div>
+                <Logo/>
+                <div className={s.linkContainer}><a href={""}>Dashboard</a></div>
+                <div className={s.linkContainer}><a href={""}>Calendar</a></div>
+                <div className={s.linkContainer}><a href={""}>Homework</a></div>
+                <div className={s.linkContainer}><a href={""}>Message</a></div>
+            </div>
+            <div>
+                <div className={s.linkContainer}><a href={""}>Settings</a></div>
+                <div className={s.linkContainer}><a href={""}>Help</a></div>
+            </div>
         </div>
     )
 }
