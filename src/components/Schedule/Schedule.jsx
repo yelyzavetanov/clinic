2a9294331d831b56edf329bb66da8dffc61043f8
yelyzavetanov <yelyzavetanov@gmail.com>
2024-04-12 10:34:01 +0300

    Make schedule header

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import s from "./Shedule.module.css";
+// import arrowIcon from "../../img/arrow.svg";
+import arrowIcon from "./../../img/system-solid-11-arrow-up.png";
+import ScheduleHeader from "./ScheduleHeader/ScheduleHeader";
 
 function Schedule() {
     return (
         <div className={s.scheduleContainer}>
             <div className={s.schedule}>
-                SCHEDULE
+                <ScheduleHeader/>
             </div>
         </div>
     )
