4cd8e907d5327fe7a100869e01e34116f0bdf1fd
yelyzavetanov <yelyzavetanov@gmail.com>
2024-04-15 10:33:47 +0300

    Fix bug with icons

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import s from "./Shedule.module.css";
-// import arrowIcon from "../../img/arrow.svg";
-import arrowIcon from "./../../img/system-solid-11-arrow-up.png";
 import ScheduleHeader from "./ScheduleHeader/ScheduleHeader";
 import WeekScale from "./WeekScale/WeekScale";
 import TimeScale from "./TimeScale/TimeScale";
