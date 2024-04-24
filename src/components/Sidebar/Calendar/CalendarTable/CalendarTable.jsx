e3cc3cab31c8983ca8d97d7b5558d35c33864976
Куріпко Владислав 1АКІТР-23б <kuripkovlad@gmail.com>
2024-04-24 10:45:21 +0300

    Refactor

--- src/components/Sidebar/Calendar/CalendarTable/CalendarTable.jsx
+++ src/components/Sidebar/Calendar/CalendarTable/CalendarTable.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import s from "./CalendarTable.module.css";
 import {generateSchedule} from "../../../../scheduleGenerator/scheduleGenerator";
 import ScheduleColumn from "../../../Schedule/ScheduleTable/ScheduleColumn/ScheduleColumn";
+import CalendarRow from "./CalendarRow/CalendarRow";
+import {generateCalendar} from "../../../../scheduleGenerator/calendarGenerator";
 
 function CalendarTable() {
     const calendarArray = generateCalendar();
 
     return (
         <div className={s.scheduleTable}>
-            {scheduleArray.map(e =>
-                <ScheduleColumn dayInfo={e} key={scheduleArray.indexOf(e)}/>
+            {calendarArray.map(e =>
+                <CalendarRow week={e} key={calendarArray.indexOf(e)}/>
             )}
         </div>
     )
 }
+
+export default CalendarTable;
