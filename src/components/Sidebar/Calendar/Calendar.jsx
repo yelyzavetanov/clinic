4e704af2d8cd5d2a901f66c1ec623921ab3b74e0
yelyzavetanov <yelyzavetanov@gmail.com>
2024-04-24 10:32:21 +0300

    Refactor: improve css layout, make simple calendar generator

--- src/components/Sidebar/Calendar/Calendar.jsx
+++ src/components/Sidebar/Calendar/Calendar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Calendar.module.css';
 import CalendarWeek from "./CalendarWeek/CalendarWeek";
+import CalendarTable from "./CalendarTable/CalendarTable";
 
 function Calendar() {
     return (
@@ -17,6 +18,7 @@ function Calendar() {
                     </div>
             </div>
             <CalendarWeek/>
+            <CalendarTable/>
         </div>
     );
 }
--- src/components/Sidebar/Calendar/CalendarTable/CalendarTable.jsx
+++ src/components/Sidebar/Calendar/CalendarTable/CalendarTable.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import s from "./CalendarTable.module.css";
 import {generateSchedule} from "../../../../scheduleGenerator/scheduleGenerator";
 import ScheduleColumn from "../../../Schedule/ScheduleTable/ScheduleColumn/ScheduleColumn";
+import {generateCalendar} from "../../../../scheduleGenerator/calendarGenerator";
 
 function CalendarTable() {
     const calendarArray = generateCalendar();
+    console.log(calendarArray)
 
     return (
         <div className={s.scheduleTable}>
-            {scheduleArray.map(e =>
-                <ScheduleColumn dayInfo={e} key={scheduleArray.indexOf(e)}/>
-            )}
+            {/*{scheduleArray.map(e =>*/}
+            {/*    <ScheduleColumn dayInfo={e} key={scheduleArray.indexOf(e)}/>*/}
+            {/*)}*/}
+            calendar table
         </div>
     )
 }
+
+export default CalendarTable;
