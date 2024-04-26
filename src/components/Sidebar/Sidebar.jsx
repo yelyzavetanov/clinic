45337904cfe2549568ead99fb70dec2b60c7ab1d
Куріпко Владислав 1АКІТР-23б <kuripkovlad@gmail.com>
2024-04-26 09:30:36 +0300

    Create add reception form

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import s from "./Sidebar.module.css";
 import Calendar from "./Calendar/Calendar";
+import AddReception from "./Recaption/AddReception";
 
 function Sidebar() {
     return (
         <div className={s.sidebar}>
             <Calendar/>
+            <div className={s.receptionLine}></div>
+            <AddReception/>
         </div>
     )
 }
