27fafb6fa63edeb90ab5b80c56c3421c7a5ec789
yelyzavetanov <yelyzavetanov@gmail.com>
2024-04-30 20:38:59 +0300

    Add basic navigation (switch between patients and schedule)

--- src/App.js
+++ src/App.js
@@ -6,26 +6,32 @@ import Header from "./components/Header/Header";
 import {useState} from "react";
 import Patients from "./components/Patients/Patients";
 import Footer from "./components/Footer/Footer";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
     const [isFullMainMenuShown, setIsFullMainMenuShown] = useState(true);
 
-
     return (
-        <div className={isFullMainMenuShown ? s.fullApp : s.app}>
-            <div className={s.menu}>
-                <MainMenu isFullMainMenuShown={isFullMainMenuShown} setIsFullMainMenuShown={setIsFullMainMenuShown}/>
-            </div>
-            <div className={s.mainContent}>
-                <Header/>
-                <div className={s.content}>
-                    {/*<Patients/>*/}
-                    <Schedule/>
-                    <Sidebar/>
+        <BrowserRouter>
+            <div className={isFullMainMenuShown ? s.fullApp : s.app}>
+                <div className={s.menu}>
+                    <MainMenu isFullMainMenuShown={isFullMainMenuShown}
+                              setIsFullMainMenuShown={setIsFullMainMenuShown}/>
                 </div>
-                <Footer/>
+                <div className={s.mainContent}>
+                    <Header/>
+                    <div className={s.content}>
+                        <Routes>
+                            <Route path={"/Patients"} element={<Patients/>}/>
+                            <Route path={"/"} element={<Schedule/>}/>
+                        </Routes>
+                        <Sidebar/>
+                    </div>
+                    <Footer/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
+
     );
 }
 
--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -4,6 +4,7 @@ import Logo from "../common/Logo/Logo";
 import scheduleIcon from "../../icons/system-regular-23-calendar.svg";
 import patientsIcon from "../../icons/system-regular-19-book.svg";
 import accountIcon from "../../icons/system-regular-63-settings-cog.svg";
+import {NavLink} from "react-router-dom";
 
 function MainMenu(props) {
     return (
@@ -15,17 +16,23 @@ function MainMenu(props) {
                         <div onClick={() => props.setIsFullMainMenuShown(false)}>
                             <Logo isFullLogoShown={true}/>
                         </div>
-                        <div className={s.linkContainer}>
+                        <NavLink
+                            to={"/"}
+                            className={({isActive}) => isActive ? s.selectedLinkContainer : s.linkContainer}
+                        >
                             <img src={scheduleIcon}/>
-                            <a href={""}>Schedule</a>
-                        </div>
-                        <div className={s.linkContainer}>
+                            <a>Schedule</a>
+                        </NavLink>
+                        <NavLink
+                            to={"/patients"}
+                            className={({isActive}) => isActive ? s.selectedLinkContainer : s.linkContainer}
+                        >
                             <img src={patientsIcon}/>
-                            <a href={""}>Patients</a>
-                        </div>
+                            <a>Patients</a>
+                        </NavLink>
                         <div className={s.linkContainer}>
                             <img src={accountIcon}/>
-                            <a href={""}>Account</a>
+                            <a>Account</a>
                         </div>
                     </div>
                 </div>
