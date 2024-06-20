f63fcf2a4a4fbb48c7ee2819a7feeffa1b63ab72
yelyzavetanov <yelyzavetanov@gmail.com>
2024-06-20 19:52:31 +0300

    Try to connect database

--- server/index.js
+++ server/index.js
@@ -4,12 +4,20 @@ const patientRoutes = require("./routes/patients");
 const userRoutes = require("./routes/users");
 const clinicRoutes = require("./routes/clinic");
 const scheduleRoutes = require("./routes/schedule");
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 const PORT = process.env.PORT || 5000;
 
