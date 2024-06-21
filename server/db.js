ffa9c4ee3e6891c00b7ae59723df1373341215c2
yelyzavetanov <yelyzavetanov@gmail.com>
2024-06-21 15:51:18 +0300

    Connect database

--- server/db.js
+++ server/db.js
@@ -6,10 +6,15 @@ const pool = mysql.createPool({
     // password: '',
     // database: 'clinic',
 
-    host: 'if0_36755545_clinic',
-    user: 'if0_36755545',
-    password: 'Buzia549',
-    database: 'if0_36755545_clinic',
+    // host: 'if0_36755545_clinic',
+    // user: 'if0_36755545',
+    // password: 'Buzia549',
+    // database: 'if0_36755545_clinic',
+
+    host: 'localhost',
+    user: 'admin',
+    password: 'admin',
+    database: 'clinic',
 
     waitForConnections: true,
     connectionLimit: 10,
