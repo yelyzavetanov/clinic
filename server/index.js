93ed86a834244ad346840af624db8617c6cc26b6
yelyzavetanov <yelyzavetanov@gmail.com>
2024-06-22 14:15:37 +0300

    Edit cors on server

--- server/index.js
+++ server/index.js
@@ -9,20 +9,12 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 
 
 const PORT = process.env.PORT || 5000;
 
-// Налаштування статичних файлів з папки 'build'
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// Обслуговування головної сторінки для всіх запитів
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-//
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
