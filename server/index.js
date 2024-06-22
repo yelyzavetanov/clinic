077bbac00f5ef01514b6a3e626c4a4c464083c05
yelyzavetanov <yelyzavetanov@gmail.com>
2024-06-22 17:01:47 +0300

    Edit cors on server

--- server/index.js
+++ server/index.js
@@ -9,16 +9,17 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({origin: '*', credentials: true}));
+// app.use(cors({origin: '*', credentials: true}));
+app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Або '*' якщо дозволяєте доступ з будь-якої адреси
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*'); // Або '*' якщо дозволяєте доступ з будь-якої адреси
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// });
 
 
 const PORT = process.env.PORT || 5000;
